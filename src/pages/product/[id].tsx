08a1d8e2d6bae7c5a5e8ffc6b2994e8882590aed
Luc <lucred@protonmail.com>
2023-04-05 10:48:53 -0300

    feat: Cover image

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -76,7 +76,7 @@ export default function Product({ product }: ProductProps) {
 
 export const getStaticPaths: GetStaticPaths = async () => {
   return {
-    paths: [{ params: { id: "prod_MipJic08JjCWHi" } }],
+    paths: [],
     fallback: true,
   };
 };
